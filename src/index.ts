711e6b272f62819462507d20b6caf78c2b7e67bc
sww <moannas@163.com>
2022-06-13 18:12:29 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+
+console.log("李奶奶啊？？？？？？？？？？？？？？？？？？？？")
+const fetch = require('node-fetch');
+console.log("李奶奶啊-------------------");
+
+console.log(fetch);
+console.log("李奶奶啊-------------------");
+
+import {
+    SpecificationExtension,//规范扩展
+    getExtension,//获取扩展
+    addExtension,//增加扩展
+    getPath,//获取path
+    isReferenceObject,//是否参考对象
+    isSchemaObject,//是否架构对象
+    Server,//服务
+    ServerVariable,//服务器变量
+    OpenApiBuilder,//生成器
+} from "openapi3-ts/dist/index";
+
+console.log({
+    SpecificationExtension,
+    getExtension,
+    addExtension,
+    getPath,
+    isReferenceObject,
+    isSchemaObject,
+    Server,
+    ServerVariable,
+    OpenApiBuilder,
+})
